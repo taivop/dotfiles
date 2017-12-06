11af6288a4f13163cea4bca467d5e3ba9e340362
taivop <taivo.pungas@gmail.com>
2017-12-06 21:16:48 +0200

    gitignore_global

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -sv "/Users/taivo/.dotfiles/runcom/.bash_profile" ~
 ln -sv "/Users/taivo/.dotfiles/runcom/.inputrc" ~
 ln -sv "/Users/taivo/.dotfiles/git/.gitconfig" ~
+ln -sv "/Users/taivo/.dotfiles/git/.gitignore_global" ~
